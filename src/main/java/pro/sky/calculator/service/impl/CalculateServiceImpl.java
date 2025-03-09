d7c714ba99b9fe67af3c54af6913f3f4000a6a5e
VolkySe <sergej_lykov@mail.ru>
2025-03-09 17:42:08 +0300

    Controller, service, methods added. Homework done.

---
+++ src/main/java/pro/sky/calculator/service/impl/CalculateServiceImpl.java
@@ -0,0 +1,26 @@
+package pro.sky.calculator.service.impl;
+
+import org.springframework.stereotype.Service;
+import pro.sky.calculator.service.CalculatorService;
+@Service
+public class CalculateServiceImpl implements CalculatorService {
+    @Override
+    public Integer plus(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public Integer minus(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public Integer multiply(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public Integer divide(Integer num1, Integer num2) {
+        return num1 / num2;
+    }
+}
